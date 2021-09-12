11e42ad170c6f2b0b0fb6ed49c07e8b9b48550f5
arc-venom <55178976+umarkhalil19@users.noreply.github.com>
2021-09-12 20:53:43 +0700

    update_guest_menu  | 12-09-2021 | UK

--- application/views/pages/v_pasien_uji.php
+++ application/views/pages/v_pasien_uji.php
@@ -9,11 +9,25 @@
     <div id="page-inner">
         <div class="row">
             <div class="col-md-12">
+                <div class="alert alert-danger">
+                    <label>Solusi</label>
+                    <ul>
+                        <li>Suspek - Melapor dan memeriksakan langsung ke pusat kesehatan masyarakat atau rumah sakit atau fasyankes lainnya agar dapat mendapatkan pemeriksaan lebih lanjut berupa pengambilan pesimen/swab dan melakukan isolasi 10 hari setelah melakukan pemeriksaan. Dan tetap mendapat pemantauan yang dilakukan petugas kesehatan</li><br>
+                        <li>Non Suspek - Melakukan pemeriksaan ke pusat kesehatan masyarakat atau rumah sakit untuk mendapatkan pemeriksaan lebih lanjut. Dan tetap mematuhi protokol kesehatan</li><br>
+                        <li>Kontak Erat - Melaporkan kepada Petugas COVID-19, melakukan karantina selama 14 hari dan mendapat pemantauan dari petugas COVID-19. Jika setelah 14 hari tidak muncul gejala maka pemantauan dihentikan, namun jika muncul gejala maka harus segera diisolasi dan mendapat pemeriksaan swab (RT-PCR)</li>
+                    </ul>
+                </div>
                 <!-- Advanced Tables -->
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         Tabel Data Pasien Yang Akan diuji
-                        <a href="<?= base_url('Data_uji/data_uji_add') ?>" class="btn btn-md btn-success pull-right"><i class="fa fa-plus"> Tambah Data</i></a>
+                        <?php
+                        if ($this->session->userdata('level') == 99) {
+                        ?>
+                            <a href="<?= base_url('Data_uji/data_uji_add') ?>" class="btn btn-md btn-success pull-right"><i class="fa fa-plus"> Tambah Data</i></a>
+                        <?php } else { ?>
+                            <a href="<?= base_url('Data_uji/data_uji_add') ?>" class="btn btn-md btn-success pull-right"><i class="fa fa-plus"> Screening</i></a>
+                        <?php } ?>
                     </div>
                     <div class="panel-body">
                         <div class="table-responsive">
@@ -24,7 +38,11 @@
                                         <th width="35%" rowspan="2">Gejala</th>
                                         <th width="5%" colspan="2">Diagnosa</th>
                                         <th rowspan="2">Pengujian Algortima</th>
-                                        <th rowspan="2">Action</th>
+                                        <?php
+                                        if ($this->session->userdata('level') == 99) {
+                                        ?>
+                                            <th rowspan="2">Action</th>
+                                        <?php } ?>
                                     </tr>
                                     <tr>
                                         <th>Naive Bayes</th>
@@ -76,14 +94,14 @@
                                                 <a href="<?= base_url('Data_uji/naive_bayes/' . $u->pasien_uji_id) ?>" class="btn btn-sm btn-primary">Naive Bayes</a> |
                                                 <a href="<?= base_url('Data_uji/dempster_shafer/' . $u->pasien_uji_id) ?>" class="btn btn-sm btn-primary">Dempster Shafer</a>
                                             </td>
-                                            <td>
-                                                <a href="<?= base_url('Data_uji/data_uji_edit/' . $u->pasien_uji_id) ?>" class="btn btn-sm btn-primary" title="Edit Data"><small class="fa fa-wrench"></small></a>
-                                                <?php
-                                                if ($this->session->userdata('level') == 99) {
-                                                ?>
+                                            <?php
+                                            if ($this->session->userdata('level') == 99) {
+                                            ?>
+                                                <td>
+                                                    <a href="<?= base_url('Data_uji/data_uji_edit/' . $u->pasien_uji_id) ?>" class="btn btn-sm btn-primary" title="Edit Data"><small class="fa fa-wrench"></small></a>
                                                     <a href="<?= base_url('Data_uji/data_uji_delete/' . $u->pasien_uji_id) ?>" class="btn btn-sm btn-danger" title="Hapus Data"><small class="fa fa-trash-o"></small></a>
-                                                <?php } ?>
-                                            </td>
+                                                </td>
+                                            <?php } ?>
                                         </tr>
                                     <?php endforeach ?>
                                 </tbody>
